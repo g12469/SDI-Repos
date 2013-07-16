27ca8d4ff26bd6fd4ab977ff09f23cc8ca491013
Sifa Tulikihihifo <sifatulikihihifo@gmail.com>
2013-07-15 21:55:35 -0700

    A few changes using the GitHub App

--- tulikihihifo_sifa_project2.js
+++ tulikihihifo_sifa_project2.js
@@ -1,1 +1,83 @@
-alert("JavaScript works!");
+// Sifa Tulikihihifo
+// SDI 1307
+// Project 2
+// Hopefully I get a good grade for this project!
+var soundWhenHungry = "Wah... Wah... WAAAAAAAAH!",
+	daughtersName = ["Lia", "Jayla"],
+	whatToFeedBaby = ["baby food", "milk", "cookies", "vegies"],
+	howMuchMoneyIHave = "$" + 450,
+;
+	
+
+/*
+1. Variables
+	a. String
+	b. Number
+	c. Array
+	
+2. Procedure
+	-> Argument
+	-> Procedure
+	-> Argument Conditional
+		-> True 
+			-> Output
+		-> False 
+			-> Output
+	-> Back
+		
+3. Boolean Function
+	- Argument: Is Lia hungry? Yes
+	- Argument: Is Jayla hungry? Yes
+		-> Boolean Funtion
+			-> Comparsion
+				-> True
+					-> Output
+				-> False
+					-> Output
+	-> Return Boolean
+
+4. Number Function
+	-> Number
+	-> Number Function
+	-> Local Variable
+	-> While Loop
+		-> True
+			-> Math
+				-> Output
+		-> False
+			-> Return Number
+			
+5. String Function
+	- String
+	- String
+		-> String Function
+		-> Local Variables
+		-> String Concatenation
+		-> Return String
+		
+6. Array Function
+	- Number
+	- Array
+		-> Array Function
+		-> Local Variables
+		-> For Loop
+			-> True
+				-> Math
+					-> Output
+			-> False
+				-> Return Array
+
+7. Returned Values
+
+8. Output
+
+FINISHED!!!!
+
+*/
+
+	
+	
+	
+		
+		
+
