adc9e72728099895e88d00a2383b4a23648a3101
Sifa Tulikihihifo <sifatulikihihifo@gmail.com>
2013-08-14 18:55:25 -0700

    Laying the foundation for how my story and code should look like... just fill in the blanks with my variables etc.

--- SDI-Project-2/rename-me.js
+++ SDI-Project-2/rename-me.js
@@ -1,1 +1,76 @@
-alert("JavaScript works!");
+// Sifa Tulikihihifo
+// SDI 1308
+// Project 2
+// Time to Workout!!!
+
+// Variables
+	
+
+// Procedure
+console.log("I wake up at " + timeToWakeUp + ". This way I have enough time to eat breakfast and get to the gym by " + timeToWorkout + ".");
+
+// Boolean Function
+var getGreetingByName = function(){
+	var currentHour = (new Date()).getHours(),
+		timeOfDay,
+		greeting;
+	if (currentHour < 12) { timeOfDay = "morning";}
+	else if (currentHour <17) { timeOfDay = "afternoon";}
+	else { timeOfDay = "evening";}
+	greeting = "Good " + timeOfDay + ", " + name + "!";
+	return greeting;
+	
+};
+
+
+// Number Function
+var getGreetingByName = function(){
+	var currentHour = (new Date()).getHours(),
+		timeOfDay,
+		greeting;
+	if (currentHour < 12) { timeOfDay = "morning";}
+	else if (currentHour <17) { timeOfDay = "afternoon";}
+	else { timeOfDay = "evening";}
+	greeting = "Good " + timeOfDay + ", " + name + "!";
+	return greeting;
+	
+};
+
+// String Function
+var getGreetingByName = function(){
+	var currentHour = (new Date()).getHours(),
+		timeOfDay,
+		greeting;
+	if (currentHour < 12) { timeOfDay = "morning";}
+	else if (currentHour <17) { timeOfDay = "afternoon";}
+	else { timeOfDay = "evening";}
+	greeting = "Good " + timeOfDay + ", " + name + "!";
+	return greeting;
+	
+};
+
+
+// Array Function
+var getGreetingByName = function(){
+	var currentHour = (new Date()).getHours(),
+		timeOfDay,
+		greeting;
+	if (currentHour < 12) { timeOfDay = "morning";}
+	else if (currentHour <17) { timeOfDay = "afternoon";}
+	else { timeOfDay = "evening";}
+	greeting = "Good " + timeOfDay + ", " + name + "!";
+	return greeting;
+	
+};
+
+
+// Returned Values
+
+var fullGreeting = getGreetingByName("Sifa");
+
+
+// Output
+console.log(fullGreeting);
+
+
+// Finish
