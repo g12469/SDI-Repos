cea9c68133eb2b1935ca5fd1dcf6ca4ac34a1503
Sifa Tulikihihifo <sifatulikihihifo@gmail.com>
2013-08-20 11:21:22 -0700

    Declaring a few variables

--- SDI_Project_3/tulikihihifo_sifa_project3.js
+++ SDI_Project_3/tulikihihifo_sifa_project3.js
@@ -1,1 +1,40 @@
-alert("JavaScript works!");
+// Sifa Tulikihihifo
+// SDI 1307
+// Project 3
+// "Let's Get It!!!"
+
+// Global Variables
+var say = function(message) {
+	console.log(message); 
+};// Won't add this function to the flowchart. Using function as a shortcut.
+var	knightInfo,
+	princessInfo,
+	trollInfo,
+	finalBattle;
+
+// Objects	
+knightInfo = {
+	name: "Anthony",
+	nickNames: ["Anthony The Great", "Prince of the West"],
+	power: 100,
+	speed: 85,
+	health: 850,
+	gold: 1000,
+	attackWithSword: 20
+};
+princessInfo = {
+	name: "Zinnia",
+	nickNames: ["The Fair Princess", "Princess of the East"],
+	power: 25,
+	speed: 25,
+	health: 500
+};
+trollInfo = {
+	name: "Zurlorg",
+	nickNames: ["The Wicked Troll", "The Ferocious Beast"],
+	power: 200,
+	speed: 40,
+	health: 1750,
+	gold: 25,
+	attackWithClaws: 14
+};
